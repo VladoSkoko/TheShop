f3a5d2352ec5e578e17efa2472e7c0da5c11f2fa
Vlado Skoko <vskoko@cxera.com>
2023-02-20 00:14:02 +0100

    Add basic API response for the BuyArticle Action.

---
+++ Shop.WebApi/Common/Models/ApiResponse.cs
@@ -0,0 +1,17 @@
+﻿namespace Shop.WebApi.Common.Models
+{
+    public interface IApiResponse
+    {
+        bool Success { get; }
+    }
+
+    public class ApiResponse : IApiResponse
+    {
+        public bool Success { get; }
+
+        public ApiResponse(bool success)
+        {
+            this.Success = success;
+        }
+    }
+}
